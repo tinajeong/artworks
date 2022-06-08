292257258b0927dbb5bf55590ebcc2a1c69ee6a2
Tina <migujeong@gmail.com>
2022-06-08 22:41:40 +0900

    migrate code and configuration to vercel template

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: "media",
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
